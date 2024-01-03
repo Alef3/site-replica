81b1eae2fff1620e062b66a9a507245c9b14a739
Álef Pereira <alefr9@gmail.com>
2024-01-02 22:42:53 -0300

    Commit final

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,12 @@
 
 
 var navbar = document.getElementById("navbar");
+var changeNavColor = document.getElementById("change-nav-color");
+var clicked =document.getElementById("clicked");
+
+var colorNavbarBefore=navbar.style.color;
+
+var clickedBefore = clicked.style.color;
 
 function toggleResponsividade() {
   
@@ -8,7 +14,10 @@ function toggleResponsividade() {
 
   if (navbar.className === "links") {
     navbar.className += " responsive";
+    clicked.style.backgroundColor ="#1a008b36";
+
   } else {
     navbar.className = "links";
+    clicked.style.backgroundColor = clickedBefore;
   }
 }
